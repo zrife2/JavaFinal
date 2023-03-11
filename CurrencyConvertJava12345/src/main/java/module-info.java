6486a6864f6e14759a9eee4c394c779f4ca97b6f
zrife2 <117114596+zrife2@users.noreply.github.com>
2023-03-11 17:43:50 -0500

    Add files via upload

---
+++ CurrencyConvertJava12345/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.mycompany.currency {
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires java.desktop;
+    requires java.logging;
+    exports com.mycompany.currency;
+}
